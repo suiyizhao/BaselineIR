638cc2104be92009f942f1b1a15a88c52a5274cd
Merander <72084387+suiyizhao@users.noreply.github.com>
2023-11-04 17:31:35 +0800

    Update options.py

--- src/options.py
+++ src/options.py
@@ -14,23 +14,32 @@ class TrainOptions():
         # ---------------------------------------- step 2/5 : data loading... ------------------------------------------------
         self.parser.add_argument("--data_source", type=str, default='', required=True, help="dataset root")
         self.parser.add_argument("--train_bs", type=int, default=4, help="size of the training batches (train_bs per GPU)")
-        self.parser.add_argument("--val_bs", type=int, default=2, help="size of the validating batches (val_bs per GPU)")
+        self.parser.add_argument("--val_bs", type=int, default=1, help="size of the validating batches (val_bs per GPU)")
         self.parser.add_argument("--crop", type=int, default=384, help="image size after cropping")
         self.parser.add_argument("--num_workers", type=int, default=4, help="number of cpu threads to use during batch generation")
         
         # ---------------------------------------- step 3/5 : model defining... ------------------------------------------------
         self.parser.add_argument("--data_parallel", action='store_true', help="if specified, training by data paralleling")
         self.parser.add_argument("--pretrained", type=str, default=None, help="pretrained model path")
+        
         self.parser.add_argument("--num_res", type=int, default=8, help="number of resblocks after each convolution")
         self.parser.add_argument("--base_channel", type=int, default=32, help="number of output channels for first convolution")
         
         # ---------------------------------------- step 4/5 : requisites defining... ------------------------------------------------
         self.parser.add_argument("--lr", type=float, default=0.0001, help="learning rate")
         self.parser.add_argument("--n_epochs", type=int, default=2000, help="number of epochs of training")
+
+        self.parser.add_argument("--beta1", type=float, default=0.9, help="hyper-parameter beta1 for Adam optimizer")
+        self.parser.add_argument("--beta2", type=float, default=0.999, help="hyper-parameter beta2 for Adam optimizer")
+        
+        self.parser.add_argument("--decay_timestamp", type=int, nargs='+', default=[500,1000,1500,2000], help="list of lr decay timestamp for multi step lr scheduler")
+        self.parser.add_argument("--decay_rate", type=float, default=0.5, help="lr decay rate for multi step lr schedule")
         
         # ---------------------------------------- step 5/5 : training... ------------------------------------------------
-        self.parser.add_argument("--print_gap", type=int, default=40, help="the gap between two print operations, in iteration")
-        self.parser.add_argument("--val_gap", type=int, default=40, help="the gap between two validations, also the gap between two saving operation, in epoch")
+        self.parser.add_argument("--print_gap", type=int, default=20, help="the gap between two print operations, in iteration")
+        self.parser.add_argument("--val_gap", type=int, default=20, help="the gap between two validations, also the gap between two saving operation, in epoch")
+        
+        self.parser.add_argument("--lambda_fft", type=float, default=0.1, help="the fft loss weight for training")
     
     def parse(self, show=True):
         opt = self.parser.parse_args()
