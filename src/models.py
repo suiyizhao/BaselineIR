a99253cc5056d4b997cdc504379998990e72f504
Merander <72084387+suiyizhao@users.noreply.github.com>
2024-03-08 17:23:43 +0800

    Update models.py

--- src/models.py
+++ src/models.py
@@ -1,137 +1,242 @@
-import math
+# ------------------------------------------------------------------------
+# Thanks to the provided baseline: https://github.com/megvii-research/NAFNet
+# ------------------------------------------------------------------------
+
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-class BasicConv(nn.Module):
-    def __init__(self, in_channel, out_channel, kernel_size, stride, bias=True, norm=False, active=True, transpose=False):
-        super(BasicConv, self).__init__()
-        if bias and norm:
-            bias = False
+from models_local import Local_Base
 
-        padding = kernel_size // 2
-        layers = list()
-        if transpose:
-            padding = kernel_size // 2 -1
-            layers.append(nn.ConvTranspose2d(in_channel, out_channel, kernel_size, padding=padding, stride=stride, bias=bias))
-        else:
-            layers.append(
-                nn.Conv2d(in_channel, out_channel, kernel_size, padding=padding, stride=stride, bias=bias))
-        if norm:
-            layers.append(nn.InstanceNorm2d(out_channel))
-        if active:
-            layers.append(nn.GELU())
-        self.main = nn.Sequential(*layers)
+class LayerNormFunction(torch.autograd.Function):
+
+    @staticmethod
+    def forward(ctx, x, weight, bias, eps):
+        ctx.eps = eps
+        N, C, H, W = x.size()
+        mu = x.mean(1, keepdim=True)
+        var = (x - mu).pow(2).mean(1, keepdim=True)
+        y = (x - mu) / (var + eps).sqrt()
+        ctx.save_for_backward(y, var, weight)
+        y = weight.view(1, C, 1, 1) * y + bias.view(1, C, 1, 1)
+        return y
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        eps = ctx.eps
+
+        N, C, H, W = grad_output.size()
+        y, var, weight = ctx.saved_variables
+        g = grad_output * weight.view(1, C, 1, 1)
+        mean_g = g.mean(dim=1, keepdim=True)
+
+        mean_gy = (g * y).mean(dim=1, keepdim=True)
+        gx = 1. / torch.sqrt(var + eps) * (g - y * mean_gy - mean_g)
+        return gx, (grad_output * y).sum(dim=3).sum(dim=2).sum(dim=0), grad_output.sum(dim=3).sum(dim=2).sum(
+            dim=0), None
+
+class LayerNorm2d(nn.Module):
+
+    def __init__(self, channels, eps=1e-6):
+        super(LayerNorm2d, self).__init__()
+        self.register_parameter('weight', nn.Parameter(torch.ones(channels)))
+        self.register_parameter('bias', nn.Parameter(torch.zeros(channels)))
+        self.eps = eps
 
     def forward(self, x):
-        return self.main(x)
+        return LayerNormFunction.apply(x, self.weight, self.bias, self.eps)
 
-class ResBlock(nn.Module):
-    def __init__(self, out_channel):
-        super(ResBlock, self).__init__()
-        self.main = nn.Sequential(
-            BasicConv(out_channel, out_channel, kernel_size=3, stride=1, active=True),
-            BasicConv(out_channel, out_channel, kernel_size=3, stride=1, active=False)
+class SimpleGate(nn.Module):
+    def forward(self, x):
+        x1, x2 = x.chunk(2, dim=1)
+        return x1 * x2
+
+class NAFBlock(nn.Module):
+    def __init__(self, c, DW_Expand=2, FFN_Expand=2, drop_out_rate=0.):
+        super().__init__()
+        dw_channel = c * DW_Expand
+        self.conv1 = nn.Conv2d(in_channels=c, out_channels=dw_channel, kernel_size=1, padding=0, stride=1, groups=1, bias=True)
+        self.conv2 = nn.Conv2d(in_channels=dw_channel, out_channels=dw_channel, kernel_size=3, padding=1, stride=1, groups=dw_channel,
+                               bias=True)
+        self.conv3 = nn.Conv2d(in_channels=dw_channel // 2, out_channels=c, kernel_size=1, padding=0, stride=1, groups=1, bias=True)
+        
+        # Simplified Channel Attention
+        self.sca = nn.Sequential(
+            nn.AdaptiveAvgPool2d(1),
+            nn.Conv2d(in_channels=dw_channel // 2, out_channels=dw_channel // 2, kernel_size=1, padding=0, stride=1,
+                      groups=1, bias=True),
         )
-        
-    def forward(self, x):
-        return self.main(x) + x
 
-class EBlock(nn.Module):
-    def __init__(self, out_channel, num_res=8):
-        super(EBlock, self).__init__()
+        # SimpleGate
+        self.sg = SimpleGate()
 
-        layers = [ResBlock(out_channel) for _ in range(num_res)]
+        ffn_channel = FFN_Expand * c
+        self.conv4 = nn.Conv2d(in_channels=c, out_channels=ffn_channel, kernel_size=1, padding=0, stride=1, groups=1, bias=True)
+        self.conv5 = nn.Conv2d(in_channels=ffn_channel // 2, out_channels=c, kernel_size=1, padding=0, stride=1, groups=1, bias=True)
 
-        self.layers = nn.Sequential(*layers)
+        self.norm1 = LayerNorm2d(c)
+        self.norm2 = LayerNorm2d(c)
 
-    def forward(self, x):
-        return self.layers(x)
+        self.dropout1 = nn.Dropout(drop_out_rate) if drop_out_rate > 0. else nn.Identity()
+        self.dropout2 = nn.Dropout(drop_out_rate) if drop_out_rate > 0. else nn.Identity()
 
-class DBlock(nn.Module):
-    def __init__(self, out_channel, num_res=8):
-        super(DBlock, self).__init__()
+        self.beta = nn.Parameter(torch.zeros((1, c, 1, 1)), requires_grad=True)
+        self.gamma = nn.Parameter(torch.zeros((1, c, 1, 1)), requires_grad=True)
 
-        layers = [ResBlock(out_channel) for _ in range(num_res)]
-        self.layers = nn.Sequential(*layers)
+    def forward(self, inp):
+        x = inp
 
-    def forward(self, x):
-        return self.layers(x)
+        x = self.norm1(x)
 
-class UNet(nn.Module):
-    def __init__(self, num_res = 8, base_channel = 32):
-        super(UNet, self).__init__()
-        
-        self.Encoder = nn.ModuleList([
-            EBlock(base_channel, num_res),
-            EBlock(base_channel*2, num_res),
-            EBlock(base_channel*4, num_res)
-        ])
+        x = self.conv1(x)
+        x = self.conv2(x)
+        x = self.sg(x)
+        x = x * self.sca(x)
+        x = self.conv3(x)
 
-        self.feat_extract = nn.ModuleList([
-            BasicConv(3, base_channel, kernel_size=3, active=True, stride=1),
-            BasicConv(base_channel, base_channel*2, kernel_size=3, active=True, stride=2),
-            BasicConv(base_channel*2, base_channel*4, kernel_size=3, active=True, stride=2),
-            BasicConv(base_channel*4, base_channel*2, kernel_size=4, active=True, stride=2, transpose=True),
-            BasicConv(base_channel*2, base_channel, kernel_size=4, active=True, stride=2, transpose=True),
-            BasicConv(base_channel, 3, kernel_size=3, active=False, stride=1)
-        ])
+        x = self.dropout1(x)
 
-        self.Decoder = nn.ModuleList([
-            DBlock(base_channel * 4, num_res),
-            DBlock(base_channel * 2, num_res),
-            DBlock(base_channel, num_res)
-        ])
+        y = inp + x * self.beta
 
-        self.Convs = nn.ModuleList([
-            BasicConv(base_channel * 4, base_channel * 2, kernel_size=1, active=True, stride=1),
-            BasicConv(base_channel * 2, base_channel, kernel_size=1, active=True, stride=1)
-        ])
+        x = self.conv4(self.norm2(y))
+        x = self.sg(x)
+        x = self.conv5(x)
 
-    def forward(self, x):
-        
-        x, h_pad, w_pad = self.pre_pad(x, 4)
-        
-        z = self.feat_extract[0](x)
-        res1 = self.Encoder[0](z)
+        x = self.dropout2(x)
 
-        z = self.feat_extract[1](res1)
-        res2 = self.Encoder[1](z)
+        return y + x * self.gamma
 
-        z = self.feat_extract[2](res2)
-        z = self.Encoder[2](z)
 
-        z = self.Decoder[0](z)
-        z = self.feat_extract[3](z)
+class NAFNet(nn.Module):
 
-        z = torch.cat([z, res2], dim=1)
-        z = self.Convs[0](z)
-        z = self.Decoder[1](z)
-        z = self.feat_extract[4](z)
+    def __init__(self, img_channel=3, width=16, middle_blk_num=1, enc_blk_nums=[], dec_blk_nums=[]):
+        super().__init__()
 
-        z = torch.cat([z, res1], dim=1)
-        z = self.Convs[1](z)
-        z = self.Decoder[2](z)
-        z = self.feat_extract[5](z)
-        
-        out = z+x
-        
-        out = self.post_crop(out, h_pad, w_pad)
-        
-        return out
+        self.intro = nn.Conv2d(in_channels=img_channel, out_channels=width, kernel_size=3, padding=1, stride=1, groups=1,
+                              bias=True)
+        self.ending = nn.Conv2d(in_channels=width, out_channels=img_channel, kernel_size=3, padding=1, stride=1, groups=1,
+                              bias=True)
+
+        self.encoders = nn.ModuleList()
+        self.decoders = nn.ModuleList()
+        self.middle_blks = nn.ModuleList()
+        self.ups = nn.ModuleList()
+        self.downs = nn.ModuleList()
+
+        chan = width
+
+        for num in enc_blk_nums:
+            self.encoders.append(
+                nn.Sequential(
+                    *[NAFBlock(chan) for _ in range(num)]
+                )
+            )
+            self.downs.append(
+                nn.Conv2d(chan, 2*chan, 2, 2)
+            )
+            chan = chan * 2
+
+        self.middle_blks = \
+            nn.Sequential(
+                *[NAFBlock(chan) for _ in range(middle_blk_num)]
+            )
+
+        for num in dec_blk_nums:
+            self.ups.append(
+                nn.Sequential(
+                    nn.Conv2d(chan, chan * 2, 1, bias=False),
+                    nn.PixelShuffle(2)
+                )
+            )
+            chan = chan // 2
+            self.decoders.append(
+                nn.Sequential(
+                    *[NAFBlock(chan) for _ in range(num)]
+                )
+            )
+
+        self.padder_size = 2 ** len(self.encoders)
+
+    def forward(self, inp):
+        B, C, H, W = inp.shape
+        inp = self.check_image_size(inp)
+
+        x = self.intro(inp)
+
+        encs = []
+
+        for encoder, down in zip(self.encoders, self.downs):
+            x = encoder(x)
+            encs.append(x)
+            x = down(x)
+
+        x = self.middle_blks(x)
+
+        for decoder, up, enc_skip in zip(self.decoders, self.ups, encs[::-1]):
+            x = up(x)
+            x = x + enc_skip
+            x = decoder(x)
+
+        x = self.ending(x)
+        x = x + inp
+
+        return x[:, :, :H, :W]
+
+    def check_image_size(self, x):
+        _, _, h, w = x.size()
+        mod_pad_h = (self.padder_size - h % self.padder_size) % self.padder_size
+        mod_pad_w = (self.padder_size - w % self.padder_size) % self.padder_size
+        x = F.pad(x, (0, mod_pad_w, 0, mod_pad_h))
+        return x
     
-    def pre_pad(self, x, pad_pow):
-        
-        _, _, h, w = x.shape
-        h_pad = pad_pow - h % pad_pow if not h % pad_pow == 0 else 0
-        w_pad = pad_pow - w % pad_pow if not w % pad_pow == 0 else 0
-        x = F.pad(x, (0, w_pad, 0, h_pad), 'replicate')
-        
-        return x, h_pad, w_pad
+class NAFNetLocal(Local_Base, NAFNet):
+    def __init__(self, *args, train_size=(1, 3, 256, 256), fast_imp=False, **kwargs):
+        Local_Base.__init__(self)
+        NAFNet.__init__(self, *args, **kwargs)
+
+        N, C, H, W = train_size
+        base_size = (int(H * 1.5), int(W * 1.5))
+
+        self.eval()
+        with torch.no_grad():
+            self.convert(base_size=base_size, train_size=train_size, fast_imp=fast_imp)
+
+
+if __name__ == '__main__':
+    def print_para_num(model):
     
-    def post_crop(self, out, h_pad, w_pad):
-        
-        _, _, h, w = out.shape
-        out = out[:,:,0:h-h_pad,0:w-w_pad]
-        
-        return out
+        '''
+        function: print the number of total parameters and trainable parameters
+        '''
+
+        total_params = sum(p.numel() for p in model.parameters())
+        total_trainable_params = sum(p.numel() for p in model.parameters() if p.requires_grad)
+        print('total parameters: %d' % total_params)
+        print('trainable parameters: %d' % total_trainable_params)
+    
+    img_channel = 3
+    width = 32
+
+    # enc_blks = [2, 2, 4, 8]
+    # middle_blk_num = 12
+    # dec_blks = [2, 2, 2, 2]
+
+    enc_blks = [1, 1, 1, 28]
+    middle_blk_num = 1
+    dec_blks = [1, 1, 1, 1]
+    
+    net = NAFNet(img_channel=img_channel, width=width, middle_blk_num=middle_blk_num,
+                      enc_blk_nums=enc_blks, dec_blk_nums=dec_blks)
+
+    print_para_num(net)
+    
+    inp_shape = (3, 256, 256)
+
+    from ptflops import get_model_complexity_info
+
+    macs, params = get_model_complexity_info(net, inp_shape, verbose=False, print_per_layer_stat=False)
+
+    params = float(params[:-3])
+    macs = float(macs[:-4])
+
+    print(macs, params)
