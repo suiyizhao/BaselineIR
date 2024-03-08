8f077be9c43f3cbc82f8d862869522e3af0594d5
Merander <72084387+suiyizhao@users.noreply.github.com>
2024-03-08 17:24:00 +0800

    Update options.py

--- src/options.py
+++ src/options.py
@@ -15,30 +15,33 @@ class TrainOptions():
         self.parser.add_argument("--data_source", type=str, default='', required=True, help="dataset root")
         self.parser.add_argument("--train_bs", type=int, default=4, help="size of the training batches (train_bs per GPU)")
         self.parser.add_argument("--val_bs", type=int, default=1, help="size of the validating batches (val_bs per GPU)")
-        self.parser.add_argument("--crop", type=int, default=384, help="image size after cropping")
-        self.parser.add_argument("--num_workers", type=int, default=4, help="number of cpu threads to use during batch generation")
+        self.parser.add_argument("--crop", type=int, default=256, help="image size after cropping")
+        self.parser.add_argument("--num_workers", type=int, default=8, help="number of cpu threads to use during batch generation")
         
         # ---------------------------------------- step 3/5 : model defining... ------------------------------------------------
         self.parser.add_argument("--data_parallel", action='store_true', help="if specified, training by data paralleling")
         self.parser.add_argument("--pretrained", type=str, default=None, help="pretrained model path")
         
-        self.parser.add_argument("--num_res", type=int, default=8, help="number of resblocks after each convolution")
-        self.parser.add_argument("--base_channel", type=int, default=32, help="number of output channels for first convolution")
+        self.parser.add_argument("--width", type=int, default=32, help="number of the latent dim for scale up/down")
+        self.parser.add_argument("--middle_blk_num", type=int, default=1, help="number of the middle blocks between last downsample and first upsample")
+        self.parser.add_argument("--enc_blk_nums", type=int, nargs='+', default=[1,1,1,28], help="number of the blocks in encoder of each scale")
+        self.parser.add_argument("--dec_blk_nums", type=int, nargs='+', default=[1,1,1,1], help="number of the blocks in decoder of each scale")
         
         # ---------------------------------------- step 4/5 : requisites defining... ------------------------------------------------
-        self.parser.add_argument("--lr", type=float, default=0.0001, help="learning rate")
-        self.parser.add_argument("--n_epochs", type=int, default=2000, help="number of epochs of training")
-
+        self.parser.add_argument("--lr", type=float, default=0.001, help="learning rate")
+        self.parser.add_argument("--n_epochs", type=int, default=3000, help="number of epochs of training")
+        
         self.parser.add_argument("--beta1", type=float, default=0.9, help="hyper-parameter beta1 for Adam optimizer")
         self.parser.add_argument("--beta2", type=float, default=0.999, help="hyper-parameter beta2 for Adam optimizer")
         
-        self.parser.add_argument("--decay_timestamp", type=int, nargs='+', default=[500,1000,1500,2000], help="list of lr decay timestamp for multi step lr scheduler")
-        self.parser.add_argument("--decay_rate", type=float, default=0.5, help="lr decay rate for multi step lr schedule")
+        self.parser.add_argument("--eta_min", type=float, default=1e-7, help="minimum learning rate of scheduler for training")
         
         # ---------------------------------------- step 5/5 : training... ------------------------------------------------
-        self.parser.add_argument("--print_gap", type=int, default=20, help="the gap between two print operations, in iteration")
-        self.parser.add_argument("--val_gap", type=int, default=20, help="the gap between two validations, also the gap between two saving operation, in epoch")
+        self.parser.add_argument("--print_gap", type=int, default=50, help="the gap between two print operations, in iteration")
+        self.parser.add_argument("--val_gap", type=int, default=50, help="the gap between two validations, also the gap between two saving operation, in epoch")
         
+        self.parser.add_argument("--lambda_cont", type=float, default=1., help="the content loss weight for training")
+        self.parser.add_argument("--lambda_lpips", type=float, default=0., help="the lpips loss weight for training")
         self.parser.add_argument("--lambda_fft", type=float, default=0.1, help="the fft loss weight for training")
     
     def parse(self, show=True):
@@ -76,6 +79,13 @@ class TestOptions():
         
         # ---------------------------------------- step 3/4 : model defining... ------------------------------------------------
         self.parser.add_argument("--model_path", type=str, default=None, required=True, help="pretrained model path")
+        
+        self.parser.add_argument("--train_crop", type=int, default=None, required=True, help="image size during training")
+        
+        self.parser.add_argument("--width", type=int, default=32, help="number of the latent dim for scale up/down")
+        self.parser.add_argument("--middle_blk_num", type=int, default=1, help="number of the middle blocks between last downsample and first upsample")
+        self.parser.add_argument("--enc_blk_nums", type=int, nargs='+', default=[1,1,1,28], help="number of the blocks in encoder of each scale")
+        self.parser.add_argument("--dec_blk_nums", type=int, nargs='+', default=[1,1,1,1], help="number of the blocks in decoder of each scale")
         
         # ---------------------------------------- step 4/4 : testing... ------------------------------------------------
         self.parser.add_argument("--save_image", action='store_true', help="if specified, save image when testing")
